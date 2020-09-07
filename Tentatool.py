12ba03a119a55e7294c8de102d1314bb5335a0e4
rhendre <59066685+rhendre@users.noreply.github.com>
2020-09-07 15:38:29 -0400

    Update Tentatool.py

--- Tentatool.py
+++ Tentatool.py
@@ -6,6 +6,7 @@ import maya.cmds as cmds
 windowName = 'WindowID'
 
 
+# UI for the tentacle autorigger
 def Tentacle_GUI():
     cmds.window(windowName, title='Tentacle AutoRigger', width=500)
     cmds.columnLayout(adjustableColumn=True)
@@ -14,37 +15,72 @@ def Tentacle_GUI():
     cmds.window(windowName, edit=True, width=200, height=200)
 
 
-def distance_between_two_points(p1, p2):
-    return math.sqrt(
-        ((p2[0] - p1[0]) * (p2[0] - p1[0])) + ((p2[1] - p1[1]) * (p2[1] - p1[1])) + ((p2[2] - p1[2]) * (p2[2] - p1[2])))
-
-
 def createTentacleRig(*args):
     count = 10
     ctrlArray = []
     jntArray = []
-    grpArray = []
+    follicleArray = []
     # create two locators at the start and end of the tentacle
-    start = cmds.spaceLocator(p=(cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0))
-    end = cmds.spaceLocator(p=(-cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0))
+    # start = cmds.spaceLocator(p=(cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0))
+    # end = cmds.spaceLocator(p=(-cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0))
+    # middle = cmds.spaceLocator(p=(0.0, cmds.getAttr('tentacool.translateY'), 0))
 
     # calculate the distance between start and end.
-    pos1 = cmds.xform(start, query=True, worldSpace=True, translation=True)
-    pos2 = cmds.xform(end, query=True, worldSpace=True, translation=True)
+    # pos1 = cmds.xform(start, query=True, worldSpace=True, translation=True)
+    # pos2 = cmds.xform(middle, query=True, worldSpace=True, translation=True)
+    # pos3 = cmds.xform(end, query=True, worldSpace=True, translation=True)
 
-    source = OpenMaya.MPoint(pos1[0], pos1[1], pos1[2])
-    target = OpenMaya.MPoint(pos2[0], pos2[1], pos2[2])
+    # startPoint = OpenMaya.MPoint(pos1[0], pos1[1], pos1[2])
+    # midPoint = OpenMaya.MPoint(pos2[0], pos2[1], pos2[2])
+    # endPoint = OpenMaya.MPoint(pos3[0], pos3[1], pos3[2])
 
-    # tentacle width
-    width = source - target
     steps = 1.0 / (count - 1)
     perc = 0
 
-    # create nurbs plane
-    nurbsPlane = cmds.nurbsPlane(axis=(0, 1, 0), width=10, lengthRatio=(1.0 / 10), u=count, v=1, degree=3, ch=0)[0]
+    # create a curve between the locators
+    curvePoints = cmds.curve(d=2, p=[(cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0),
+                                     (0, cmds.getAttr('tentacool.translateY'), 0),
+                                     (-cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0)])
+    curveName = cmds.rename(curvePoints, 'Tentacle_curve')
 
-    for x in range(count):
-        # create hair follicles for the tentacle for better twisting motion
+    cmds.select(curveName + '.cv[*]')
+    allPts = cmds.ls(sl=True, fl=True)
+
+    # for each vertices create a cluster and a locator and then parent the cluster to the locator
+    for cv in allPts:
+        i = 0
+        clusterName = cmds.cluster(cv, n='cluster' + str(i + 1))
+        position = cmds.xform(cv, q=True, t=True, worldSpace=True)
+        locName = cmds.spaceLocator(n='Tentacle_Locator')
+        cmds.xform(r=True, t=position)
+        cmds.parent(clusterName, locName)
+        i = i + 1
+
+    # create joints on the curve
+
+    curveLocator = cmds.spaceLocator(n='Curve_Locator')
+    cmds.select(curveLocator, curveName)
+    pathLocator = cmds.pathAnimation(stu=1, etu=1000, f=True)
+
+    # set a motion path
+    cmds.selectKey(pathLocator + '_uValue', k=True, time=(1, 1000))
+
+    # make motion path linear
+    cmds.keyTangent(itt='Linear', ott='Linear')
+
+    jointNumber = 10
+    divFactor = 1000 / jointNumber
+    index = 0
+    cmds.select(cl=True)
+    temp = cmds.currentTime(1, e=True)
+
+    createJointOnCurve(curveLocator, divFactor, index, jntArray, temp)
+
+    # create follicle, controls around the created joints
+    index = 0
+    for joints in jntArray:
+        index += 1
+        # create hair follicles and connect it to the joints
         follicle = cmds.createNode('follicle')
         follicleTransform = cmds.listRelatives(follicle, type='transform', p=True)
         cmds.connectAttr(follicle + '.outRotate', follicleTransform[0] + '.rotate')
@@ -53,90 +89,65 @@ def createTentacleRig(*args):
         cmds.connectAttr('tentacool.worldMatrix', follicle + '.inputWorldMatrix')
         cmds.connectAttr('tentacool.outMesh', follicle + '.inputMesh')
 
-        cmds.setAttr(follicle + '.parameterU', 10 * x)
+        cmds.setAttr(follicle + '.parameterU', cmds.getAttr('tentacle_jnt' + str(index) + '.translateX'))
         cmds.setAttr(follicle + '.parameterV', cmds.getAttr('tentacool.translateY'))
 
-        cmds.parentConstraint(start, end, follicleTransform, mo=True)
+        cmds.parentConstraint(joints,  follicleTransform, mo=True)
+
+
 
         # create follicle group
-        follicle_grp = cmds.group(name=('follicle' + 'group' + str(x + 1)), em=1)
+        follicle_grp = cmds.group(name=('follicle' + 'group' + str(index)), em=1)
         cmds.parent(follicle, follicle_grp)
+        follicleArray.append(follicle_grp)
 
+        # create controls corresponding to the joints and follicles
         # create the controls
-        ctrl = cmds.circle(c=(0, 0, 0), nr=(1, 0, 0), r=5.0, ch=0, name=('tentacle_ctrl' + str(x + 1)))[0]
+        ctrl = cmds.circle(c=(0, 0, 0), nr=(1, 0, 0), r=5.0, ch=0, name=('tentacle_ctrl' + str(index)))[0]
         ctrlArray.append(ctrl)
 
-        #create control group
-        ctrl_grp = cmds.group(name = ('ctrl_group'+ str(x+1)), em = 1)
+        # create control group
+        ctrl_grp = cmds.group(name=('ctrl_group' + str(index)), em=1)
         cmds.parent(ctrl, ctrl_grp)
-
-        # create joints
-        joints = cmds.joint(name=('tentacle' + str(x + 1) + '_jnt'), radius=0.3)  # Create a new joint.
-        jntArray.append(joints)
-
-        # parent joints to create a chain
-        # if x != 0:
-        # cmds.delete(cmds.parentConstraint('tentacle' + str(x + 1) + '_jnt', 'tentacle' + str(x) + '_jnt', mo = True))
-
-        # display local axis for the joints
-        cmds.setAttr('tentacle' + str(x + 1) + '_jnt' + ".displayLocalAxis", True)
 
         # add color to the controls
         cmds.setAttr((cmds.listRelatives(ctrl, type='shape')[0] + '.overrideEnabled'), 1)
         cmds.setAttr((cmds.listRelatives(ctrl, type='shape')[0] + '.overrideColor'), 14)
 
-        # equidistantly place the joints in the tentacle
-        cmds.setAttr((ctrl + '.translate'), 20 * x, cmds.getAttr('tentacool.translateY'), 0)
-        cmds.setAttr((joints + '.translate'), 20 * x, cmds.getAttr('tentacool.translateY'), 0)
-
-        # parent constraint controls to joints
-        cmds.parent(ctrl_grp, follicle_grp)
-        cmds.parent(joints, ctrl_grp)
-
-        cmds.parentConstraint(start, follicle_grp, weight = 1- perc)
-        cmds.parentConstraint(end, follicle_grp, weight = perc)
+        addConstraints(ctrl, ctrl_grp, follicle_grp, jntArray, joints, perc)
 
         perc += steps
 
-    # create controllers at the start end and mid points
-    top_Point = \
-        cmds.circle(c=(cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0), nr=(1, 0, 0),
-                    r=10.0,
-                    ch=0, name=('top_ctrl'))[0]
-    middle_Point = cmds.circle(c=(0, 0, 0), nr=(1, 0, 0), r=10.0, ch=0, name=('mid_ctrl'))[0]
-    end_Point = \
-        cmds.circle(c=(-cmds.getAttr('tentacool.translateX'), cmds.getAttr('tentacool.translateY'), 0), nr=(1, 0, 0),
-                    r=10.0,
-                    ch=0, name=('end_ctrl'))[0]
-
-    addColor(end_Point, middle_Point, top_Point)
-
-    midConst = cmds.pointConstraint(top_Point, end_Point, middle_Point)
-
-    cmds.delete(cmds.parentConstraint(start, jntArray, weight=1.0 - perc, mo=False))
-    cmds.delete(cmds.parentConstraint(end, jntArray, weight=perc, mo=False))
-
-    # creating non linear deformers
-
-    twist = cmds.nonlinearDeformer(type='bend', curvature=0.5)
+        # add a non linear deformer for sine functionality
 
 
-def addColor(end_Point, middle_Point, top_Point):
-    cmds.setAttr((cmds.listRelatives(top_Point, type='shape')[0] + '.overrideEnabled'), 1)
-    cmds.setAttr((cmds.listRelatives(top_Point, type='shape')[0] + '.overrideColor'), 14)
-    cmds.setAttr((cmds.listRelatives(middle_Point, type='shape')[0] + '.overrideEnabled'), 1)
-    cmds.setAttr((cmds.listRelatives(middle_Point, type='shape')[0] + '.overrideColor'), 14)
-    cmds.setAttr((cmds.listRelatives(end_Point, type='shape')[0] + '.overrideEnabled'), 1)
-    cmds.setAttr((cmds.listRelatives(end_Point, type='shape')[0] + '.overrideColor'), 14)
+  ctrl_grp = cmds.group(name=('tentacle_rig'), em=1)
+  cmds.parent(follicleArray, ctrl_grp)
 
 
-def addAttributes():
-    cmds.addAttr('top_ctrl', longName='Twist', niceName='twist', at="float", k=True)
-    cmds.setAttr(('top_ctrl' + '.' + 'Twist'), lock=1)
-    cmds.addAttr('top_ctrl', longName='Roll', niceName='roll', at="float", k=True)
-    cmds.setAttr(('top_ctrl' + '.' + 'Roll'), lock=1)
-    cmds.addAttr('top_ctrl', longName='Stretch', niceName='stretch', at="float", k=True)
-    cmds.setAttr(('top_ctrl' + '.' + 'Stretch'), lock=1)
+def addConstraints(ctrl, ctrl_grp, follicle_grp, jntArray, joints, perc):
+    # parent constraint controls to joints
+
+    cmds.parent(ctrl_grp, follicle_grp)
+    cmds.parent(joints, ctrl)
+    cmds.parentConstraint(joints, ctrl, mo=False)
+    cmds.parentConstraint('Tentacle_Locator', joints, weight=1 - perc, mo=False)
+    cmds.parentConstraint('Tentacle_Locator1', joints, weight=0, mo=False)
+    cmds.parentConstraint('Tentacle_Locator2', joints, weight=perc, mo=False)
+    # cmds.delete(cmds.parentConstraint(start, jntArray, weight=1.0 - perc, mo=False))
+    # cmds.delete(cmds.parentConstraint(end, jntArray, weight=perc, mo=False))
+
+
+def createJointOnCurve(curveLocator, divFactor, index, jntArray, temp):
+    # loop to create a joint on the curve
+    for temp in range(0, 1050, int(temp + divFactor)):
+        index += 1
+        cmds.currentTime(temp, e=True)
+
+        locPos = cmds.xform(curveLocator, q=True, ws=True, t=True)
+        joints = cmds.joint(n='tentacle_jnt' + str(index), a=True, p=(locPos[0], locPos[1], locPos[2]))
+        cmds.setAttr('tentacle_jnt' + str(index) + ".displayLocalAxis", True)
+        jntArray.append(joints)
 
 
 Tentacle_GUI()
